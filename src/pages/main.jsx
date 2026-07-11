2622bbc99fe3fffa2e027ca3c7b5736093b561e0
Camarot1 <tupojdoter@gmail.com>
2026-07-11 22:53:27 +0300

    readmi

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export default function MainPage(){
     const navigate = useNavigate()
     useEffect( ()=> {
-        navigate("/login")
+        navigate("/tasks")
     }, [navigate])
     return(
         <div>Редирект</div>
